0145581f96dde5b68f94609cd81a7e6cee361f24
Abhinav Garg <91721449+theabhiinavgarg@users.noreply.github.com>
2022-10-03 10:08:55 +0530

    past events

---
+++ pastevents/PastEvents.js
@@ -0,0 +1,26 @@
+// gsap.to("#bg", {
+//   scrollTrigger: {
+//     scrub: true,
+//   },
+//   y: 200,
+//   scale: 1.2,
+// });
+
+gsap.to("#simran", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 1130,
+  // y: 500,
+  // y: -1800,
+  scale: 0.8,
+});
+
+gsap.to("#train", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 1000,
+//   y: 200,
+  scale: 0.7,
+});
